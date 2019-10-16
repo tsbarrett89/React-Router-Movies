d1bf75f2f468d7f02566594949ce60c72c50adf6
Thomas Barrett <tsbarrett89@gmail.com>
2019-10-15 19:39:25 -0600

    finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 import SavedList from './Movies/SavedList';
 
@@ -13,6 +16,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route exact path="/" render={props => <MovieList {...props} />} />
+      <Route path="/movies/:id" render={props => <Movie {...props} />} /> 
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const MovieList = props => {
@@ -30,6 +31,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
+    <Link to={`/movies/${movie.id}`} >
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -46,6 +48,7 @@ function MovieDetails({ movie }) {
         </div>
       ))}
     </div>
+    </Link>
   );
 }
 
